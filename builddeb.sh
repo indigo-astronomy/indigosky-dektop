ba80a36aabd8955e17fe436be72901bee4208bf6
Rumen G.Bogdanovski <rumen@skyarchive.org>
2021-04-19 11:11:36 +0300

    Initial commit

---
+++ builddeb.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+VERSION=0.1
+
+DEBFULLNAME="Rumen Bogdanovski"
+EMAIL="rumen@skyarchive.org"
+
+__check_file_exits() {
+    [ ! -f ${1} ] && { echo "file '${1}' not found"; exit 1; }
+}
+
+# Check for Debian package building executables and tools.
+[ ! $(which dch) ] && { echo "executable 'dch' not found install package: 'devscripts'"; exit 1; }
+[ ! $(which dpkg-buildpackage) ] && { echo "executable 'dpkg-buildpackage' not found install package: 'dpkg-dev'"; exit 1; }
+[ ! $(which cdbs-edit-patch) ] && { echo "executable 'cdbs' not found install package: 'cdbs'"; exit 1; }
+
+# Create entry in debian/changelog.
+dch --create --package "indigosky-desktop" --newversion ${VERSION} --distribution unstable --nomultimaint -t "Build from official upstream."
+
+# Finally build the package.
+dpkg-buildpackage \-us \-uc \-I.git \-I\*.out[0-9]\* \-I\*.swp
